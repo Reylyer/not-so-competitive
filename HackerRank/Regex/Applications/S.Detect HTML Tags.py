1ef7be9e15f993ebdbe65b641100a6c98dec0fc8
Reylyer <reylyercross@gmail.com>
2022-11-01 23:11:54 +0700

    ini baru hari ini ngerjain regex html

---
+++ HackerRank/Regex/Applications/S.Detect HTML Tags.py
@@ -0,0 +1,16 @@
+import re
+
+
+htmlstr = ""
+res = []
+regex = r"((?<=\<)(?:\s*)\/?(\w+)(?:.*?)(?=\>))"
+
+    
+for _ in range(int(input())):
+    htmlstr += input()
+    
+print(";".join(sorted(set([ a[1] for a in re.findall(pattern=regex, string=htmlstr, flags=re.X|re.S)]))))
+
+# just for fun
+# import re
+# print(";".join(sorted(set([ a[1] for a in re.findall(pattern=r"((?<=\<)\s*\/?(\w+).*?(?=\>))", string="\n".join([input() for _ in range(int(input()))]), flags=re.X|re.S)]))))
